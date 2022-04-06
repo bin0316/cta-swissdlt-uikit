4a16efec6dfb710fbb210f35be54f564327dfbc5
Blockchain Developer <55833057+bin0316@users.noreply.github.com>
2022-04-07 00:53:31 +0800

    V1

--- src/widgets/Menu/components/Logo.tsx
+++ src/widgets/Menu/components/Logo.tsx
@@ -35,7 +35,7 @@ const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href }) => {
   const isAbsoluteUrl = href.startsWith("http");
   const innerLogo = (
     <>
-      <Text fontSize="23px"> CrossTheAge </Text>
+      <Text fontSize="23px"> Urstamm Exchange </Text>
       <LogoWithText className="desktop-icon" isDark={isDark} />
     </>
   );
@@ -50,12 +50,12 @@ const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href }) => {
         )}
       </MenuButton>
       {isAbsoluteUrl ? (
-        <StyledLink as="a" href={href} aria-label="CrossTheAge">
-          <Text fontSize="23px"> CrossTheAge </Text>
+        <StyledLink as="a" href={href} aria-label="Urstamm Exchange">
+          <Text fontSize="23px"> Urstamm Exchange </Text>
         </StyledLink>
       ) : (
-        <StyledLink to={href} aria-label="CrossTheAge">
-          <Text fontSize="23px"> CrossTheAge </Text>
+        <StyledLink to={href} aria-label="Urstamm Exchange">
+          <Text fontSize="23px"> Urstamm Exchange </Text>
         </StyledLink>
       )}
     </Flex>
